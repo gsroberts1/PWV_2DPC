ceb79c6b7d254a0747b18512fe6489ce6c1fca03
Grant Roberts <gsroberts@wisc.edu>
2021-10-06 15:54:47 -0500

    Preprocessing
    Changed several preprocessing functions and added several more.

--- Preprocessing_functions/loadCartesian2DPC.m
+++ Preprocessing_functions/loadCartesian2DPC.m
@@ -1,5 +1,3 @@
-function DICOMS = readDICOMS(directory)
-
 dirinfo = [dir('*.dcm'); dir('*.dicom'); dir('*.sdcopen')];
 if isempty(dirinfo)
     disp('NO DICOMS FOUND IN DIRECTORY');
@@ -9,11 +7,11 @@ header = dicominfo(dirinfo(1).name);
 for file = 1:length(dirinfo)
     DICOMS(:,:,file) = dicomread(dirinfo(file).name);
 end 
+mag = DICOMS(:,:,41:end);
+vz = DICOMS(:,:,1:40);
+
 spatialRes = header.PixelSpacing(1);
 temporalRes = header.NominalInterval/header.CardiacNumberOfImages;
 disp(['Spatial Resolution = ' num2str(spatialRes) ' mm']);
 disp(['Temporal Resolution = ' num2str(temporalRes) ' ms']);
-
-
-end
-
+clear dirinfo file header 
