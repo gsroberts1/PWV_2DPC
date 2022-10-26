d2810fdf33996386945e900eb5abd9c057ed6627
Grant Roberts <gsroberts@wisc.edu>
2022-10-26 08:58:37 -0500

    Wed Oct 26 08:58:36 CDT 2022

--- Preprocessing_functions/unwrapFrames.m
+++ Preprocessing_functions/unwrapFrames.m
@@ -1,34 +1,39 @@
-clean;
-load2DPC;
-frame = 18;
-plane = v(:,:,3,frame);
+% Load data, enter wrapped frames, set Venc
+loadRadial2DPC;
+frames = 20:29;
+Venc = 1500;
 
+% Draw ROI for all frames
 figure; imshow(MAG,[]);
 circle = drawcircle;
 radius = circle.Radius; 
 center = round(circle.Center);
-
-[X,Y] = ndgrid(1:size(plane,1),1:size(plane,2));
+[X,Y] = ndgrid(1:size(MAG,1),1:size(MAG,2));
 X = X-center(2); %shift coordinate grid
 Y = Y-center(1);
 roiMask = sqrt(X.^2+Y.^2)<=radius;
-newPlane = plane.*roiMask;
-%newPlane = newPlane(center(2)-30:center(2)+29,center(1)-30:center(1)+29);
-newPlane = (newPlane/1500)*pi;
-%newROI = roiMask(center(2)-30:center(2)+29,center(1)-30:center(1)+29);
-UW = Unwrap_TIE_DCT_Iter(newPlane);
-UW = (UW/pi)*1500;
-%UW = UW-3000;
-%UW(UW==-3000) = 0;
-%UW = newROI.*UW;
-%UW = padarray(UW,[226 226]);
-figure; imshowpair(newPlane,UW,'montage');
 
-plane = plane.*(~roiMask);
-plane = plane + UW;
+% Unwrap each frame
+for i=1:length(frames)
+    frame = frames(i);
+    plane = vz(:,:,frame);
+    
+    newPlane = plane.*roiMask;
+    newPlane = (newPlane/Venc)*pi;
+    UW = Unwrap_TIE_DCT_Iter(newPlane);
+    UW = (UW/pi)*Venc;
+    %UW = UW-3000;
+    %UW(UW==-3000) = 0;
+    %UW = newROI.*UW;
+    %UW = padarray(UW,[226 226]);
+    figure; imshowpair(newPlane,UW,'montage');
 
-figure; imshow(plane,[]);
-filename = ['ph_' num2str(frame-1,'%03.f') '_vd_3.dat'];
-fid = fopen(filename,'w');
-fwrite(fid,plane,'int16');
-fclose(fid);
+    plane = plane.*(~roiMask);
+    plane = plane + UW;
+
+    % Overwrite existing dat file
+    filename = ['ph_' num2str(frame-1,'%03.f') '_vd_3.dat'];
+    fid = fopen(filename,'w');
+    fwrite(fid,plane,'int16');
+    fclose(fid);
+end 
